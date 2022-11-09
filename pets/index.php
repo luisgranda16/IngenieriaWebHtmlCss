73d63b6c13fcf90d59f43abccf113b77f678fc2f
luisgranda16 <56703398+luisgranda16@users.noreply.github.com>
2022-11-08 22:34:09 -0500

    Actualizacion2

---
+++ pets/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+    <link rel="icon" href="images/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="images/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+<div class="contenedor">
+	
+	<header class="cabeceraPrincipal">
+		<section class="logotipo"><img src="images/logotipoPet.png"></section>
+		<nav class="menuPrincipal">
+			<a href="#">Inicio</a>
+			<a href="internas/servicios.php">Servicios</a>
+			<a href="#">Productos</a>
+			<a href="#">Adopción</a>
+			<a href="#">Contactos</a>
+		</nav>
+	</header>
+	
+	<section class="sliderPrincipal">
+		<img src="images/slider1.jpg" title="Slider">
+	</section>
+	
+	<section class="titulo"><h3 class="colorAzul mayusculas">Adopción</h3></section>
+	
+	<section class="adopcion1">
+		<img src="images/dog1.jpg" title="Firulais" descrition="Perro en adopcion">
+		<article class="card1">
+			<h3 class="mayusculas colorAzul">Firulais</h3>
+			<p class="derecha">Snares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat tradeSnares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat trade</p>
+			<div class="derecha">
+				<button class="button button1">Más Información</button>
+			</div>
+		</article>
+	</section>
+	
+	<section class="adopcion2">
+		<img src="images/dog2.jpg" title="Bingo">
+		<article class="card1">
+			<h3 class="mayusculas colorAzul">Bingo</h3>
+			<p>Snares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat tradeSnares are silent killers laid by poachers across wild places to trap and kill animals for the illegal bushmeat trade</p>
+			<div class="derecha">
+				<button class="button button1">Más Información</button>
+			</div>
+		</article>
+	</section>
+
+	<section class="doctor">
+		<div class="cuadro-doctor1">
+			<img src="images/doctor1.jpg">
+			<div class="texto-encima">
+				<h3 class="doctor-texto">Dr. Marta Gomez</h3>
+			</div>
+		</div>
+		<div class="cuadro-doctor1">
+			<img src="images/doctor2.jpg">
+			<div class="texto-encima">
+			<h3 class="doctor-texto">Dr. Mateo Flores</h3>
+				<p></p>
+			</div>
+		</div>
+		<div class="cuadro-doctor1">
+			<img src="images/doctor3.jpg">
+			<div class="texto-encima">
+				<h3 class="doctor-texto">Dr. Gabriela Mendoza</h3>	
+				<p></p>
+			</div>
+		</div>
+		<div class="cuadro-doctor1">
+			<img src="images/doctor4.jpg">
+			<div class="texto-encima">
+				<h3 class="doctor-texto">Dr. Jorge Masa</h3>	
+				<p></p>
+			</div>
+		</div>
+	</section>	
+		
+
+	<section class="sponsor">
+		<img src="images/logotipoPet.png" class="img1">
+		<img src="images/logo_blanco_utpl.png" class="img2">
+	</section>		
+
+	<footer class="piePagina">
+		<section class="derechos">
+			<h6>Derechos Reservados UTPL 2022</h6>
+		</section>
+		<nav class="redesSociales">
+			<a href=""><i class="fa-brands fa-facebook"></i></a>
+			<a href=""><i class="fa-brands fa-instagram"></i></a>
+			<a href=""><i class="fa-brands fa-tiktok"></i></a>
+		</nav>
+	</footer>
+
+</div>
+</body>
+</html>
